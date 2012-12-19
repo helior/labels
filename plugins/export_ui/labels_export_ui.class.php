e3f863c8c0c67700c11e344c6c159afbbf02bf20
Helior Colorado <me@helior.info>
2012-12-18 22:10:52 -0800

    Added plural form element.

--- plugins/export_ui/labels_export_ui.class.php
+++ plugins/export_ui/labels_export_ui.class.php
@@ -1,3 +1,15 @@
 <?php
 
-class labels_export_ui extends ctools_export_ui {}
+class labels_export_ui extends ctools_export_ui {
+  function edit_form(&$form, &$form_state) {
+    parent::edit_form($form, $form_state);
+
+    // Add the Plural form element.
+    $form['info']['plural'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Plural'),
+      '#size' => 24,
+      '#default_value' => $form_state['item']->plural,
+    );
+  }
+}
