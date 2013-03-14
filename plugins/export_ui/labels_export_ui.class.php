b461870b350a90ce9b22958bc58e093559b0e55b
Helior Colorado <me@helior.info>
2013-03-13 18:21:05 -0700

    Added proper label to singular field; added description about plural field.

--- plugins/export_ui/labels_export_ui.class.php
+++ plugins/export_ui/labels_export_ui.class.php
@@ -4,10 +4,13 @@ class labels_export_ui extends ctools_export_ui {
   function edit_form(&$form, &$form_state) {
     parent::edit_form($form, $form_state);
 
+    $form['info']['singular']['#title'] = t('Singular');
+
     // Add the Plural form element.
     $form['info']['plural'] = array(
       '#type' => 'textfield',
       '#title' => t('Plural'),
+      '#description' => t('This will be the plural version of the label.'),
       '#size' => 24,
       '#default_value' => $form_state['item']->plural,
     );
