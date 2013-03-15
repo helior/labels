558e212456b15d73addd167648e6331c9cf2795a
Helior Colorado <me@helior.info>
2013-03-14 17:10:39 -0700

    Running the proper submission processes to update/remove translatable strings.

--- plugins/export_ui/labels_export_ui.class.php
+++ plugins/export_ui/labels_export_ui.class.php
@@ -1,6 +1,9 @@
 <?php
 
 class labels_export_ui extends ctools_export_ui {
+
+  // Alter the edit form to add a form element for our plural string, as well as
+  // a "Save and translate" button if we're doing translations.
   function edit_form(&$form, &$form_state) {
     parent::edit_form($form, $form_state);
 
@@ -14,5 +17,24 @@ class labels_export_ui extends ctools_export_ui {
       '#size' => 24,
       '#default_value' => $form_state['item']->plural,
     );
+  }
+
+  // Altering edit form submission to update translation strings in case we're
+  // doing translations.
+  function edit_form_submit(&$form, &$form_state) {
+    parent::edit_form_submit($form, $form_state);
+
+    if (labels_translatable()) {
+      i18n_string_object_update('labels', $form_state['item']);
+    }
+  }
+
+  // If we're doing translations, make sure that we're removing i18n strings
+  // from deleted objects.
+  function delete_form_submit(&$form_state) {
+    parent::delete_form_submit($form_state);
+    if (labels_translatable() && $form_state['op'] == 'delete') {
+      i18n_string_object_remove('labels', $form_state['item']);
+    }
   }
 }
